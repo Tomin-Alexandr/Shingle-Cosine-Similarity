b92309ca76377bdeb3994fcc8a5e7095fb5f985b
AlexandrTomin <user9437@gmail.com>
2020-09-02 23:11:45 +0500

    ver 2

--- shingles.py
+++ shingles.py
@@ -9,15 +9,12 @@ from datetime import datetime
 import itertools
 import hashlib
 
-
-
-DB_Connect = pymysql.connect('localhost', 'user', '25477452', 'news', charset='utf8mb4')
+DB_Connect = pymysql.connect('localhost', 'user', '25477452', 'pikabu', charset='utf8mb4')
 cursor = DB_Connect.cursor()
 
-#cursor.execute("select lower(text),id from facebook.comments order by rand() limit 5000")
-cursor.execute("select lower(title),url from news WHERE date_published  >= '2020-08-23 00:00:00' AND date_published <= '2020-09-02 00:59:59' limit 5000")
+# cursor.execute("select lower(text),id from facebook.comments order by rand() limit 5000")
+cursor.execute("select lower(content),id from comments limit 5000")
 ddata = cursor.fetchall()
-
 
 
 def chunks(lst, count):
@@ -40,20 +37,18 @@ def Calcs(data):
             text_2 = re.sub(r'[^\w\s]', '', couple[1][0])
             other_shingles = [text_2[i:i + 5] for i in range(len(text_2))][:-5]  # shingle size = 5
 
-
             s = len(set(shingles) & set(other_shingles)) / len(set(shingles) | set(other_shingles))
             if s > 0.65 and s != 1:
-                cursor.execute("INSERT INTO ml.temp_news (url, title, rating,dublicate) VALUES (%s, %s, %s, %s)",(couple[0][1],couple[0][0],s,couple[1][0]))
+                cursor.execute("INSERT INTO result (id, content, rating,dublicate) VALUES (%s, %s, %s, %s)",
+                               (couple[0][1], couple[0][0], s, couple[1][0]))
                 DB_Connect.commit()
         except Exception as e:
             pass
 
 
-
-if name == 'main':
+if __name__ == '__main__':
     Listfor = itertools.combinations(ddata, 2)
     Data = chunks(list(Listfor), 11)
-
 
     pool = Pool(processes=11)
     start = datetime.now()
