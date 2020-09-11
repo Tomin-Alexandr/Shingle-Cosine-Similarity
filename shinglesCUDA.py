5042ab4d2ebe7c888e8cb194bbc80720245d6ce5
AlexandrTomin <user9437@gmail.com>
2020-09-11 21:18:46 +0500

    Lastworkver

--- shinglesCUDA.py
+++ shinglesCUDA.py
@@ -1,6 +1,5 @@
 import re
 from nltk.corpus import stopwords
-from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pymysql
@@ -8,11 +7,8 @@ from datetime import datetime
 import numpy as np
 import pandas as pd
 
-STOP_WORDS = set(stopwords.words('russian'))
-STOP_SYMBOLS = '.,!?:;-\n\r()/=+*"' + "'"
 
-
-def delete_bad_string(text):
+def clean_text(text):
     text = re.sub('[\f\t(\uf101)●➔·•]', '', text)  # delete wrong symbols
     text = re.sub('\n{2,}', '\n', text)  # repeat \n
     text = re.sub('( +)\n', '', text)  # empty string
@@ -26,52 +22,46 @@ def delete_bad_string(text):
     a = [item for item in a if not re.search('\d', item)]
     return ' '.join(a)
 
+STOP_WORDS = set(stopwords.words('russian'))
+STOP_SYMBOLS = '.,!?:;-\n\r()/=+*"' + "'"
 
-def canonize_words(source):
-    return [x for x in [re.sub(r'[\.,!?:\-;\n\r()/=\+\*&#%$@\'\"●•·➔]', '', y) for y in source.lower().split()] if
-            x and (x not in STOP_WORDS)]
-
-
-def clean_text(text):
-    # return canonize_words(delete_bad_string(text))
-    return delete_bad_string(text)
-
+ChunkSize = 2000
 
 DB_Connect = pymysql.connect('localhost', 'user', '25477452', 'pikabu', charset='utf8mb4')
 cursor = DB_Connect.cursor()
 
-# cursor.execute("select lower(text),id from facebook.comments order by rand() limit 5000")
-cursor.execute("select lower(content),id from comments order by id limit 55000")
-Data = cursor.fetchall()
-corpus = [item[0] for item in Data]
-corpusId = [item[1] for item in Data]
+cursor.execute("select lower(content),id from comments where tag != 'Коты&Девушки' and length(content)>100")
+w = cursor.fetchall()
+corpus = [item[0] for item in w]
+corpusId = [item[1] for item in w]
 
 start = datetime.now()
 for i in range(len(corpus)):
     corpus[i] = clean_text(corpus[i])
 
 count_vectorizer = CountVectorizer()
-
 sparse_matrix = count_vectorizer.fit_transform(corpus)
-doc_term_matrix = sparse_matrix.todense()
 
-df = pd.DataFrame(doc_term_matrix, columns=count_vectorizer.get_feature_names(), index=corpus)
-
+CorpusLen = sparse_matrix.shape[0]
 prev = 0
-count = 0
-for i in range(500, len(df.index), 500):
-    temp_corpus = corpus[prev:i]
-    temp_id = corpusId[prev:i]
-    cosMatrix = cosine_similarity(df[prev:i], df[prev:i])
-    prev = i
+
+for chunk in range(ChunkSize, CorpusLen, ChunkSize):
+    csrMatrix = sparse_matrix.tocsr()[prev:chunk - 1]
+    temp_corpus = corpus[prev:chunk - 1]
+    temp_id = corpusId[prev:chunk - 1]
+    print(prev, chunk)
+    doc_term_matrix = csrMatrix.todense()
+    df = pd.DataFrame(doc_term_matrix, columns=count_vectorizer.get_feature_names(), index=corpus[prev:chunk - 1])
+    prev = chunk
+
+    cosMatrix = cosine_similarity(df, df)
+
     x, y = np.where((cosMatrix > 0.65) & (cosMatrix < 0.99))
-
     for k, j in zip(x, y):
-        cursor.execute(
-            "INSERT INTO similarity (FirstId, FirstText, sim, SecondId, SecondText) VALUES (%s, %s, %s, %s, %s)",
-            (temp_id[k], temp_corpus[k], cosMatrix[k][j], temp_id[j], temp_corpus[j]))
+        cursor.execute("INSERT INTO similarity (FirstId,FirstText,sim,SecondId,SecondText) VALUES (%s, %s, %s, %s, %s)",
+                       (temp_id[k], temp_corpus[k], cosMatrix[k][j].item(), temp_id[j],temp_corpus[j]))
 
-    count += 1
-    if count % 100 == 0:
-        print(count)
+DB_Connect.commit()
+
+DB_Connect.close()
 print(datetime.now() - start)
